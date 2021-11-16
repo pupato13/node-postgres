ef77680aae5f8f28b1d0ee53b6b19f7cd2c4110b
Diego Pupato <pupato.diego@gmail.com>
2021-11-16 14:20:45 +1300

    move FacebookAuthenticationService to its own file

---
+++ src/data/services/facebook-authentication.ts
@@ -0,0 +1,15 @@
+import { ILoadFacebookUserApi } from "@/data/contracts/apis";
+import { AuthenticationError } from "@/domain/errors";
+import { IFacebookAuthentication } from "@/domain/features";
+
+export class FacebookAuthenticationService {
+    constructor(private readonly loadFacebookUserApi: ILoadFacebookUserApi) {}
+
+    async perform(
+        params: IFacebookAuthentication.Params
+    ): Promise<AuthenticationError> {
+        await this.loadFacebookUserApi.loadUser(params);
+
+        return new AuthenticationError();
+    }
+}
