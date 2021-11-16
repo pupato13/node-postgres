d254f05e53b2b566b1974e7d0c9ae38b860a6c88
Diego Pupato <pupato.diego@gmail.com>
2021-11-16 14:25:44 +1300

    change test strategy to use jest mocks

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,38 +1,26 @@
-import { ILoadFacebookUserApi } from "@/data/contracts/apis";
 import { FacebookAuthenticationService } from "@/data/services";
 import { AuthenticationError } from "@/domain/errors";
 
-class LoadFacebookUserApiSpy implements ILoadFacebookUserApi {
-    callsCount = 0;
-
-    token?: string;
-
-    result = undefined;
-
-    async loadUser(
-        params: ILoadFacebookUserApi.Params
-    ): Promise<ILoadFacebookUserApi.Result> {
-        this.callsCount++;
-        this.token = params.token;
-
-        return await Promise.resolve(this.result);
-    }
-}
-
 describe("FacebookAuthenticationService", () => {
     it("should call LoadFacebookUserApi with correct params", async () => {
-        const loadFacebookUserApi = new LoadFacebookUserApiSpy();
+        const loadFacebookUserApi = {
+            loadUser: jest.fn(),
+        };
         const sut = new FacebookAuthenticationService(loadFacebookUserApi);
 
         await sut.perform({ token: "any_token" });
 
-        expect(loadFacebookUserApi.token).toBe("any_token");
-        expect(loadFacebookUserApi.callsCount).toBe(1);
+        expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({
+            token: "any_token",
+        });
+        expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
     });
 
     it("should return AuthenticationError when LoadFacebookUserApi returns undefined", async () => {
-        const loadFacebookUserApi = new LoadFacebookUserApiSpy();
-        loadFacebookUserApi.result = undefined;
+        const loadFacebookUserApi = {
+            loadUser: jest.fn(),
+        };
+        loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined);
         const sut = new FacebookAuthenticationService(loadFacebookUserApi);
 
         const authResult = await sut.perform({ token: "any_token" });
