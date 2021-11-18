bb5807bfa8c0011d1f07e327339d2aa7dc4d4e39
Diego Pupato <pupato.diego@gmail.com>
2021-11-19 08:47:33 +1300

    ensure FacebookApi get app token with correct params

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -5,11 +5,20 @@ import { ILoadFacebookUserApi } from "@/data/contracts/apis";
 class FacebookApi {
     private readonly baseUrl = "https://graph.facebook.com";
 
-    constructor(private readonly httpClient: IHttpGetClient) {}
+    constructor(
+        private readonly httpClient: IHttpGetClient,
+        private readonly clientId: string,
+        private readonly clientSecret: string,
+    ) {}
 
     async loadUser(params: ILoadFacebookUserApi.Params): Promise<void> {
         await this.httpClient.get({
             url: `${this.baseUrl}/oauth/access_token`,
+            params: {
+                client_id: this.clientId,
+                client_secret: this.clientSecret,
+                grant_type: "client_credentials",
+            },
         });
     }
 }
@@ -21,18 +30,27 @@ interface IHttpGetClient {
 namespace IHttpGetClient {
     export type Params = {
         url: string;
+        params: object;
     };
 }
 
 describe("FacebookApi", () => {
+    const clientId = "any_client_id";
+    const clientSecret = "any_client_secret";
+
     it("should get app token", async () => {
         const httpClient = mock<IHttpGetClient>();
-        const sut = new FacebookApi(httpClient);
+        const sut = new FacebookApi(httpClient, clientId, clientSecret);
 
         await sut.loadUser({ token: "any_client_token" });
 
         expect(httpClient.get).toHaveBeenCalledWith({
             url: "https://graph.facebook.com/oauth/access_token",
+            params: {
+                client_id: clientId,
+                client_secret: clientSecret,
+                grant_type: "client_credentials",
+            },
         });
     });
 });
