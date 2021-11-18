d1ab00622ac27274339127cb661069c33e06a91b
Diego Pupato <pupato.diego@gmail.com>
2021-11-19 10:54:36 +1300

    ensure AxiosHttpClient rethrows if axios get throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -55,5 +55,16 @@ describe("AxiosHttpClient", () => {
 
             expect(result).toEqual("any_data");
         });
+
+        it("should rethrows is get throws", async () => {
+            fakeAxios.get.mockRejectedValue(new Error("http_error"));
+
+            const promise = sut.get({
+                url,
+                params,
+            });
+
+            await expect(promise).rejects.toThrow(new Error("http_error"));
+        });
     });
 });
