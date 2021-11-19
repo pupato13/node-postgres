1947f1fa0d333611f8a7e86ee9a3703ee9d11dde
Diego Pupato <pupato.diego@gmail.com>
2021-11-19 14:37:45 +1300

    share SUT criation

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -6,28 +6,28 @@ import { PgUserAccountRepository } from "@/infra/postgres/repos";
 import { makeFakeDb } from "@/tests/infra/postgres/mocks";
 
 describe("Pg UserAccountRepository", () => {
+    let sut: PgUserAccountRepository;
+    let pgUserRepo: Repository<PgUser>;
+    let backup: IBackup;
+
+    beforeEach(() => {
+        // Restore the empty DB backup before each test
+        backup.restore();
+        sut = new PgUserAccountRepository();
+    });
+
+    beforeAll(async () => {
+        const db = await makeFakeDb([PgUser]);
+        // Creates a backup with empty DB to restore it before each test
+        backup = db.backup();
+        pgUserRepo = getRepository(PgUser);
+    });
+
+    afterAll(async () => {
+        await getConnection().close();
+    });
+
     describe("load", () => {
-        let sut: PgUserAccountRepository;
-        let pgUserRepo: Repository<PgUser>;
-        let backup: IBackup;
-
-        beforeEach(() => {
-            // Restore the empty DB backup before each test
-            backup.restore();
-            sut = new PgUserAccountRepository();
-        });
-
-        beforeAll(async () => {
-            const db = await makeFakeDb([PgUser]);
-            // Creates a backup with empty DB to restore it before each test
-            backup = db.backup();
-            pgUserRepo = getRepository(PgUser);
-        });
-
-        afterAll(async () => {
-            await getConnection().close();
-        });
-
         it("should return an account if email exists", async () => {
             await pgUserRepo.save({ email: "any_email" });
 
