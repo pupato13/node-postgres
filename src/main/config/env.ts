459f972dc69860a23b05ff4222f7ccb2f78f3a8f
Diego Pupato <pupato.diego@gmail.com>
2021-11-25 10:19:07 +1300

    test linter husky

---
+++ src/main/config/env.ts
@@ -0,0 +1,7 @@
+export const env = {
+    facebookApi: {
+        clientId: process.env.FB_CLIENT_ID ?? "3048774682027509",
+        clientSecret:
+            process.env.FB_CLIENT_SECRET ?? "323730b96c5c16ef95686bb638510b8e",
+    },
+};
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,19 @@
+// import { FacebookApi } from "@/infra/apis";
+// import { AxiosHttpClient } from "@/infra/http";
+// import { env } from "@/main/config/env";
+
 describe("FacebookAPI", () => {
-    it("test", () => {
+    it("should return a Facebook User if token is valid", async () => {
+        // const axiosClient = new AxiosHttpClient();
+        // const sut = new FacebookApi(
+        //     axiosClient,
+        //     env.facebookApi.clientId,
+        //     env.facebookApi.clientSecret
+        // );
+
+        // const fbUser = await sut.loadUser({ token: "" });
+
+        // expect(fbUser).toEqual({ facebookId: "", email: "", name: "" });
         expect(1).toBe(1);
     });
 });
