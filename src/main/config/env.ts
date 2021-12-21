56bfe69f8a365ce0fccc46f9ca8781ae69c2aa96
Diego Pupato <pupato.diego@gmail.com>
2021-12-22 08:50:31 +1300

    refactor: move all keys to .env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,8 @@
 export const env = {
     facebookApi: {
-        clientId: process.env.FB_CLIENT_ID ?? "3048774682027509",
-        clientSecret:
-            process.env.FB_CLIENT_SECRET ?? "323730b96c5c16ef95686bb638510b8e",
+        clientId: process.env.FB_CLIENT_ID ?? "",
+        clientSecret: process.env.FB_CLIENT_SECRET ?? "",
     },
-    port: process.env.PORT ?? 8080,
-    jwtSecret: process.env.JWT_SECRET ?? "W22+Mc)uU,3$&-xq",
+    port: process.env.PORT ?? "",
+    jwtSecret: process.env.JWT_SECRET ?? "",
 };
