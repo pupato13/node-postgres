1cdbdd7c738b0039d2a938b8317f20bffb20e44a
Diego Pupato <pupato.diego@gmail.com>
2021-12-22 09:13:43 +1300

    feat: ensure ExpressMiddleware calls handle with correct request

---
+++ tests/main/adapters/express-middleware.spec.ts
@@ -0,0 +1,30 @@
+import { HttpResponse } from "@/application/helpers";
+import { getMockReq, getMockRes } from "@jest-mock/express";
+import { RequestHandler } from "express";
+import { mock } from "jest-mock-extended";
+
+type Adapter = (middleware: IMiddleware) => RequestHandler;
+
+const adaptExpressMiddleware: Adapter =
+    (middleware) => async (req, res, next) => {
+        await middleware.handle({ ...req.headers });
+    };
+
+interface IMiddleware {
+    handle: (httpRequest: any) => Promise<HttpResponse>;
+}
+
+describe("ExpressMiddleware", () => {
+    it("should call handle with correct request", async () => {
+        const req = getMockReq({ headers: { any: "any" } });
+        const res = getMockRes().res;
+        const next = getMockRes().next;
+        const middleware = mock<IMiddleware>();
+        const sut = adaptExpressMiddleware(middleware);
+
+        await sut(req, res, next);
+
+        expect(middleware.handle).toHaveBeenCalledWith({ any: "any" });
+        expect(middleware.handle).toHaveBeenCalledTimes(1);
+    });
+});
