a11fddfe64526e6133c762141e455f43aa5a48cb
Diego Pupato <pupato.diego@gmail.com>
2021-12-28 10:27:03 +1300

    feat: ensure UniqueId returns correct id 2

--- tests/infra/crypto/unique-id.spec.ts
+++ tests/infra/crypto/unique-id.spec.ts
@@ -25,4 +25,12 @@ describe("UniqueId", () => {
 
         expect(uuid).toBe("any_key_20211003101010");
     });
+
+    it("should return correct id 2", () => {
+        const sut = new UniqueId(new Date(2018, 2, 10, 18, 1, 0));
+
+        const uuid = sut.uuid({ key: "any_key" });
+
+        expect(uuid).toBe("any_key_20180310180100");
+    });
 });
