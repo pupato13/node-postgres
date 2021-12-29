f9642db123d342fc40604102c90bdfaa60825ee4
Diego Pupato <pupato.diego@gmail.com>
2021-12-30 11:13:10 +1300

    feat: ensure DeletePictureController returns 204 on success

--- tests/application/controllers/delete-profile-picture.spec.ts
+++ tests/application/controllers/delete-profile-picture.spec.ts
@@ -1,12 +1,15 @@
 import { ChangeProfilePicture } from "@/domain/use-cases";
+import { HttpResponse, noContent } from "@/application/helpers";
 
 type HttpRequest = { userId: string };
 
 class DeletePictureController {
     constructor(private readonly changeProfilePicture: ChangeProfilePicture) {}
 
-    async handle({ userId }: HttpRequest): Promise<void> {
+    async handle({ userId }: HttpRequest): Promise<HttpResponse> {
         await this.changeProfilePicture({ id: userId });
+
+        return noContent();
     }
 }
 
@@ -29,5 +32,14 @@ describe("DeletePictureController", () => {
             id: "any_user_id",
         });
         expect(changeProfilePicture).toHaveBeenCalledTimes(1);
+    });
+
+    it("should return 204 on success", async () => {
+        const httpResponse = await sut.handle({ userId: "any_user_id" });
+
+        expect(httpResponse).toEqual({
+            statusCode: 204,
+            data: null,
+        });
     });
 });
