dd8d8d084065332e6357ce1d9e381fb051e6fb91
Diego Pupato <pupato.diego@gmail.com>
2021-12-31 08:58:05 +1300

    feat: add AWS S3 integration test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,7 +2,13 @@ export const env = {
     facebookApi: {
         clientId: process.env.FB_CLIENT_ID ?? "",
         clientSecret: process.env.FB_CLIENT_SECRET ?? "",
+        accessToken: process.env.FB_ACCESS_TOKEN ?? "",
     },
     port: process.env.PORT ?? 8080,
     jwtSecret: process.env.JWT_SECRET ?? "",
+    s3: {
+        accessKey: process.env.AWS_S3_ACCESS_KEY ?? "",
+        secret: process.env.AWS_S3_SECRET ?? "",
+        bucket: process.env.AWS_S3_BUCKET ?? "",
+    },
 };
