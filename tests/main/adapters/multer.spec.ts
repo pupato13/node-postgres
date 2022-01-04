70ae1c46b373a5990263ca28ad511a4bbc83fbf2
Diego Pupato <pupato.diego@gmail.com>
2022-01-05 12:30:16 +1300

    feat: ensure MulterAdapter returns 500 if upload fails

--- tests/main/adapters/multer.spec.ts
+++ tests/main/adapters/multer.spec.ts
@@ -2,12 +2,15 @@ import multer from "multer";
 import { getMockReq, getMockRes } from "@jest-mock/express";
 import { Request, Response, NextFunction, RequestHandler } from "express";
 import { mocked } from "ts-jest/utils";
+import { ServerError } from "@/application/errors";
 
 jest.mock("multer");
 
 const adaptMulter: RequestHandler = (req, res, next) => {
     const upload = multer().single("picture");
-    upload(req, res, () => {});
+    upload(req, res, (error) => {
+        res.status(500).json({ error: new ServerError(error).message });
+    });
 };
 
 describe("MulterAdapter", () => {
@@ -22,7 +25,7 @@ describe("MulterAdapter", () => {
 
     beforeAll(() => {
         fakeMulter = multer as jest.Mocked<typeof multer>;
-        uploadSpy = jest.fn();
+        uploadSpy = jest.fn().mockImplementation(() => {});
         singleSpy = jest.fn().mockImplementation(() => uploadSpy);
         multerSpy = jest.fn().mockImplementation(() => ({ single: singleSpy }));
         mocked(fakeMulter).mockImplementation(multerSpy);
@@ -44,5 +47,20 @@ describe("MulterAdapter", () => {
         expect(singleSpy).toHaveBeenCalledTimes(1);
         expect(uploadSpy).toHaveBeenCalledWith(req, res, expect.any(Function));
         expect(uploadSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it("should return 500 if upload fails", () => {
+        const error = new Error("multer_error");
+        uploadSpy.mockImplementationOnce((req, res, next) => {
+            next(error);
+        });
+        sut(req, res, next);
+
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.status).toHaveBeenCalledTimes(1);
+        expect(res.json).toHaveBeenCalledWith({
+            error: new ServerError(error).message,
+        });
+        expect(res.json).toHaveBeenCalledTimes(1);
     });
 });
