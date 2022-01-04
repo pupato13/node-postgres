36d5572808a8291b547bfe6dd7829b47cbf15391
Diego Pupato <pupato.diego@gmail.com>
2022-01-05 12:41:20 +1300

    feat: ensure MulterAdapter calls next on success

--- tests/main/adapters/multer.spec.ts
+++ tests/main/adapters/multer.spec.ts
@@ -24,6 +24,8 @@ const adaptMulter: RequestHandler = (req, res, next) => {
                 },
             };
         }
+
+        next();
     });
 };
 
@@ -103,5 +105,12 @@ describe("MulterAdapter", () => {
                 mimeType: req.file?.mimetype,
             },
         });
+    });
+
+    it("should call next on success", () => {
+        sut(req, res, next);
+
+        expect(next).toHaveBeenCalledWith();
+        expect(next).toHaveBeenCalledTimes(1);
     });
 });
